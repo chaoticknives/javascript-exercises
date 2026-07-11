18e80239e6775a94b51c884340d03d4c7aa8f6cc
chaoticknives <205522400+chaoticknives@users.noreply.github.com>
2026-07-11 18:40:11 -0400

    Complete assignment

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
+const leapYears = function (year) {
+    const isDivisibleFour = year % 4 === 0;
+    const isCentury = year % 100 === 0;
+    const isDivisibleFourHundred = year % 400 === 0;
 
-};
+    if (isDivisibleFourHundred && (!isCentury || isDivisibleFourHundred)) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
 // Do not edit below this line
 module.exports = leapYears;
